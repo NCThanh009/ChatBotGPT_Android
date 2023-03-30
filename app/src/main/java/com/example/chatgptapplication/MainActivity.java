f8412e8000f09c1e104f7f2e6520980e59a62583
NCThanh009 <nguyencongthanh111213@gmail.com>
2023-03-30 20:00:09 +0700

    custom

--- app/src/main/java/com/example/chatgptapplication/MainActivity.java
+++ app/src/main/java/com/example/chatgptapplication/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body= RequestBody.create(jsonBody.toString(),JSON);
         Request request= new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer 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") //sk-aZumHLRww09uvo0KnrHpT3BlbkFJO2cVy2akR0jQJHbPZk9
                 .post(body)
                 .build();
         client.newCall(request).enqueue(new Callback() {
